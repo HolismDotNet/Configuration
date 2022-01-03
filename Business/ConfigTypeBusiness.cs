ac5ab4c9b51f9bccb578637b91c0efdf5718de09
Saeed Nemati <sn.1361@gmail.com>
2022-01-03 12:47:48 +0330

    committed by script

--- Business/ConfigTypeBusiness.cs
+++ Business/ConfigTypeBusiness.cs
@@ -3,7 +3,7 @@ using Holism.Configuration.Models;
 
 namespace Holism.Configuration.Business;
 
-public class TypeBusiness : EnumBusiness<Models.Type>
+public class ConfigTypeBusiness : EnumBusiness<ConfigType>
 {
     public override string ConnectionString => Repository.ConfigItem.ConnectionString;
 }
